197b387902838bd17add98009449ceb6564ddf06
JIN-LEE <jin-lee@jin-leeui-MacBookAir.local>
2021-02-28 15:31:21 +0900

    refactor(graph): variable name modification

--- graph/001-farest-node_pla-2.py
+++ graph/001-farest-node_pla-2.py
@@ -1,26 +1,26 @@
 # 정답코드
 
-
+# n : 노드의 개수, edge : 노드간 간선 관계
 def solution(n, edge):
     import collections
-    answer = [1] + [0] * (n - 1)
-    vec = [[] for i in range(n)]
-    que = collections.deque([1])
-    for i in edge:
-        vec[i[0] - 1].append(i[1])
-        vec[i[1] - 1].append(i[0])
-    while que:
-        l = len(que)
-        for i in range(l):
-            q = que.popleft()
-            for e in vec[q - 1]:
-                if answer[e - 1] == 0:
-                    answer[e - 1] = 1
-                    que.append(e)
-    return l
+    visited = [1] + [0] * (n - 1)
+    adj_list = [[] for i in range(n)]
+    queue = collections.deque([1])
+    for elem in edge:
+        adj_list[elem[0] - 1].append(elem[1])
+        adj_list[elem[1] - 1].append(elem[0])
+    while queue:
+        length = len(queue)
+        for i in range(length):
+            q = queue.popleft()
+            for e in adj_list[q - 1]:
+                if visited[e - 1] == 0:
+                    visited[e - 1] = 1
+                    queue.append(e)
+    return length
 
 
 if __name__ == '__main__':
     n = 6
-    vertex = [[3, 6], [4, 3], [3, 2], [1, 3], [1, 2], [2, 4], [5, 2]]
-    print('{0}'.format(solution(n, vertex)))
+    edge = [[3, 6], [4, 3], [3, 2], [1, 3], [1, 2], [2, 4], [5, 2]]
+    print('{0}'.format(solution(n, edge)))
