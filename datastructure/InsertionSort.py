d68a9ecb86b635390748080e68296e1a533e152a
platanus <platanus@canxan.com>
2021-03-20 02:45:59 +0900

    feat(quicksort): Make completion implement quicksort again

--- datastructure/InsertionSort.py
+++ datastructure/InsertionSort.py
@@ -6,8 +6,24 @@ def insertion_sort(data):
     for i in range(1, n):
         temp_data = data[i]
         for j in reversed(range(1, i + 1)):
-            # 몬가.. i+1에서 시작하는게 이상하겠지만 몬가..
-            # j=i에 대응하지않고 j=i-1에서 시작하는게 이상함....
+            if data[j - 1] > temp_data:
+                data[j] = data[j - 1]
+            else:
+                data[j] = temp_data
+                break
+
+        if data[0] > temp_data:
+            data[0] = temp_data
+
+    return data
+
+
+def insertion_sort_range(data, start, last):
+    # data[start], data[last] = data[last], data[start]
+
+    for i in range(start, last + 1):
+        temp_data = data[i]
+        for j in reversed(range(1, i + 1)):
             if data[j - 1] > temp_data:
                 data[j] = data[j - 1]
             else:
--- datastructure/QuickSort_002.py
+++ datastructure/QuickSort_002.py
@@ -5,54 +5,46 @@
 
 from typing import MutableSequence
 
-from datastructure import MedianOfThree, InsertionSort
+from datastructure import InsertionSort, MedianOfThree
 
 
 def quick_sort(data_array: MutableSequence, start: int, last: int) -> None:
     low = start
     high = last
-    NUM_THRESHOLD = 3
-    print('주어진 데이터: {0}'.format(data_array))
-    pivot = MedianOfThree.find_pivot_index(data_array, low, (low + high) // 2, high)
-    # pivot = data_array[(start + last) // 2]
-    print('MoT 정렬 후: {0}, pivot: {1}'.format(data_array, pivot))
+    NUM_THRESHOLD = 6
+    print('주어진 데이터: {0}, 3개의 피벗 후보 {1}, {2}, {3}'.format(data_array, data_array[low], data_array[(low + high) // 2],
+                                                         data[high]))
     if (last - start < NUM_THRESHOLD):
-        InsertionSort.insertion_sort(data_array)
+        InsertionSort.insertion_sort_range(data_array, start, last)
         print('{0}보다 작은 배열로 삽입 정렬 후: {1}'.format(NUM_THRESHOLD, data_array))
         print("-------------------")
         return data_array
-    print(f'index[{start}] ~ [{last}]: ', *data_array[start: last + 1])  # 새로 추가된 부분
+    pivot = MedianOfThree.find_pivot_index(data_array, low, (low + high) // 2, high)
+    # pivot = data_array[(start + last) // 2]
+    print('MoT 정렬 후: {0}, pivot: {1}'.format(data_array, pivot))
+    # print('{0} {1} {2}'.format(low, (low + high) // 2, high))
+
+    # print(f'index[{start}] ~ [{last}]: ', *data_array[start: last])  # 새로 추가된 부분
 
     while low <= high:
-        while data_array[low] < pivot:
-            # if low >= len(data_array): break
-            low += 1
-
-        while data_array[high] > pivot:
-            # if high < 1: break
-            high -= 1
-
+        while data_array[low] < pivot: low += 1
+        while data_array[high] > pivot: high -= 1
         if low <= high:
             data_array[low], data_array[high] = data_array[high], data_array[low]
-            # if low >= len(data_array):
             low += 1
-            # else:
-            #     low = len(data_array) - 1
-            # if high < 1:
-            #     high = 0
-            # else:
             high -= 1
-    print("-------------------")
+
     if start < high: quick_sort(data_array, start, high)
     if last > low: quick_sort(data_array, low, last)
+    print("-------------------")
     return data_array
 
 
 if __name__ == '__main__':
     # data = [4, 2, 5, 7, 9, 1, 3, 8, 6, 0]
     # data = [4, 21, 33, 42, 65, 2, 5, 7, 9, 1, 3, 8, 6, 0]
-    # data = [13, 77, 49, 35, 61, 48, 73, 23, 95, 89, 37, 57, 99, 17, 32, 94, 28, 15, 55, 7, 51, 88, 97, 62]
-    data = [13, 77, 49, 35, 61, 48, 73, 15, 55, 7, 51, 88, 97, 62]
-    # data = [13, 77, 49, 35, 7, 51, 88, 97]
+    # data = [13, 77, 49, 35, 61, 48, 73, 23, 95, 89, 37, 57, 99, 17, 32, 94, 28, 15, 55, 17, 51, 88, 97, 62]
+    data = [13, 77, 49, 35, 61, 48, 73, 15, 55, 11, 51, 88, 97, 62]
+    # data = [13, 77, 49, 35, 11, 51, 88, 97]
     last = int(len(data) - 1)
     print('{0}'.format(quick_sort(data, 0, last)))
